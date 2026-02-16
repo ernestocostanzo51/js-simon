d23f498fa000fd7a9331fcd4c737ad11ca279f9e
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-16 17:23:48 +0100

    commenti vari

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ for (let i = 0; i < pEl.length; i++) {
         let inputEl = document.createElement('input')
         inputEl.type = "number";
         inputEl.placeholder = "Inserisci il numero"
+        
 
         pEl[i].textContent = ""; 
         pEl[i].appendChild(inputEl)
@@ -36,18 +37,21 @@ for (let i = 0; i < pEl.length; i++) {
 
 setTimeout(function(){
     buttonEl.disabled=false
+   
+
 },11000)
 
 
 formEl.addEventListener('submit', function(e){
 e.preventDefault()
-
+//prendiamo tutti gli input
 const inputs = document.querySelectorAll("input")
 
 let numeri_indoviati = 0
 
 for(let i=0; i<inputs.length; i++){
-    if(parseInt(inputs[i].value) === numeriDaIndovinare[i]){
+    if(parseInt(inputs[i].value) === numeriDaIndovinare[i]){  //SE l'input preso in analisi è uguale al numero da indovinare corrispondente si
+                                                               //incrementa di 1 i numeri indovinati
         numeri_indoviati++
     }
         
@@ -56,11 +60,10 @@ for(let i=0; i<inputs.length; i++){
 h1El.textContent = "Hai indovinato " + numeri_indoviati + " su 5"
 
 })
-
+//dopo i 30 secondi il bottone non si può più usare
 setTimeout(function(){
     buttonEl.disabled=true
     h1El.textContent ="TEMPO SCADUTO"
-    inputEl.disabled=true
 }, 40000)
     
 
